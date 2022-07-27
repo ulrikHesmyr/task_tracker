1ecfe7623c7edc48925b088ed0294ca02b345632
ulrikHesmyr <ulrik.hesmyr2002@gmail.com>
2022-07-27 17:52:23 +0200

    .

---
+++ prosjekter/kundesystem/src/components/AddTask.js
@@ -0,0 +1,45 @@
+import { useState } from "react"
+
+const AddTask = ({onAdd}) => {
+
+    const [text, setText] = useState('')
+    const [day, setDay] = useState('')
+    const [reminder, setReminder] = useState(false)
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        if(!text){
+            alert('U need text')
+            return
+        }
+
+        onAdd({text, day, reminder})
+
+        setText('')
+        setDay('')
+        setReminder(false)
+    }
+  return (
+    <form className='add-form' onSubmit={(e)=>{
+        onSubmit(e)
+    }}>
+        <div className="form-control">
+            <label>Task</label>
+            <input type="text" placeholder="Add task" value={text} onChange={(e)=>setText(e.target.value)} />
+        </div>
+        <div className="form-control">
+            <label>Add day and time</label>
+            <input type="text" placeholder="Add day and time" value={day} onChange={(e)=>setDay(e.target.value)}/>
+        </div>
+        <div className="form-control form-control-check">
+            <label>Set Reminder</label>
+            <input type="checkbox" value={reminder} checked={reminder} onChange={(e)=>setReminder(e.currentTarget.checked)}/>
+        </div>
+
+        <input type="submit" value="Save task" className="btn btn-block"/>
+
+    </form>
+  )
+}
+
+export default AddTask
